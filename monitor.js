d233992b2ee1c302a6931f66212a54a40bc7698e
pleja <pleja918@gmail.com>
2021-02-18 23:52:52 -0500

    LPvalueCheck

--- monitor.js
+++ monitor.js
@@ -6,53 +6,64 @@ const poolList = require("./config.js");
 
 const ETHUSDCPool = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc";
 
-let provider = ethers.getDefaultProvider("wss://mainnet.infura.io/ws/v3/ xxxxxxxxxxxxxxxxxxxxxx ")
+let provider = ethers.getDefaultProvider("wss://mainnet.infura.io/ws/v3/ xxxxxxxxxxxxxxxx ")
 
 let topic = ethers.utils.id("nameRegistered(bytes32,address,uint256)");
 
 
 
 // LP Value to compare to
-let priceOfOneLP;
 
-// const LPValueStartup = async () => {
-//     let reserves = await liqPool.functions.getReserves();
-//     let totalLP = await liqPool.functions.totalSupply();
-//     let LPDecimal = await liqPool.functions.decimals();
-//     let USDCRes = parseFloat(reserves._reserve0.toString()) / (10 ** 6);
-//     let totalValue = USDCRes * 2;
-//     let LPs = totalLP / (10 ** LPDecimal);
-//     let pricePerLP = totalValue / LPs;
-//     priceOfOneLP = pricePerLP;
-//     // console.log(pricePerLP);
-// }
 
-// const LPValueCheck = async () => {
-//     let reserves = await liqPool.functions.getReserves();
-//     let totalLP = await liqPool.functions.totalSupply();
-//     let LPDecimal = await liqPool.functions.decimals();
-//     let USDCRes = parseFloat(reserves._reserve0.toString()) / (10 ** 6);
-//     let totalValue = USDCRes * 2;
-//     let LPs = totalLP / (10 ** LPDecimal);
-//     let pricePerLP = totalValue / LPs;
-//     if (pricePerLP >= priceOfOneLP) {
-//         let comparative = pricePerLP / priceOfOneLP;
-//         if (comparative >= 1.05) {
-//             console.log("Change of 5% up");
-//             if (comparative >= 1.10) {
-//                 console.log("change of 10% up");
-//             }
-//         }
-//     } else if (pricePerLP < priceOfOneLP) {
-//         let comparative = pricePerLP / priceOfOneLP;
-//         if (comparative <= 0.95) {
-//             console.log("change of 5% down");
-//             if (comparative <= 0.90) {
-//                 console.log("change of 10% down");
-//             }
-//         }
-//     }
-// }
+const LPValueStartup = async (liqPool, stableCoin, stableCoinDecimal) => {
+    let reserves = await liqPool.functions.getReserves();
+    let totalLP = await liqPool.functions.totalSupply();
+    let LPDecimal = 18;
+    let USDCRes;
+    if (stableCoin === 0) {
+        USDCRes = parseFloat(reserves._reserve0.toString()) / (10 ** stableCoinDecimal);
+    } else if (stableCoin === 1) {
+        USDCRes = parseFloat(reserves._reserve1.toString()) / (10 ** stableCoinDecimal);
+    }
+    let totalValue = USDCRes * 2;
+    let LPs = totalLP / (10 ** LPDecimal);
+    let pricePerLP = totalValue / LPs;
+    return pricePerLP;
+    // console.log(pricePerLP);
+}
+
+const LPValueCheck = async (liqPool, stableCoin, stableCoinDecimal, priceOfOneLP) => {
+    let reserves = await liqPool.functions.getReserves();
+    let totalLP = await liqPool.functions.totalSupply();
+    let LPDecimal = 18;
+    let USDCRes;
+    if (stableCoin === 0) {
+        USDCRes = parseFloat(reserves._reserve0.toString()) / (10 ** stableCoinDecimal);
+    } else if (stableCoin === 1) {
+        USDCRes = parseFloat(reserves._reserve1.toString()) / (10 ** stableCoinDecimal);
+    }
+    
+    let totalValue = USDCRes * 2;
+    let LPs = totalLP / (10 ** LPDecimal);
+    let pricePerLP = totalValue / LPs;
+    if (pricePerLP >= priceOfOneLP) {
+        let comparative = pricePerLP / priceOfOneLP;
+        if (comparative >= 1.05) {
+            console.log("Change of 5% up");
+            if (comparative >= 1.10) {
+                console.log("change of 10% up");
+            }
+        }
+    } else if (pricePerLP < priceOfOneLP) {
+        let comparative = pricePerLP / priceOfOneLP;
+        if (comparative <= 0.95) {
+            console.log("change of 5% down");
+            if (comparative <= 0.90) {
+                console.log("change of 10% down");
+            }
+        }
+    }
+}
 // LPValueStartup();
 
 for (let i = 0; i < poolList.length; i++) {
@@ -73,6 +84,7 @@ for (let i = 0; i < poolList.length; i++) {
         console.log("1");
     })
     if (category === "Stablecoins") {
+        let priceOfOneLP = LPValueStartup(liqPool, 0, decimal0);
         liqPool.on("Swap", async (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
             let toke0 = parseFloat(amount0In.toString());
             toke0 = toke0 / (10 ** decimal0);
@@ -94,11 +106,18 @@ for (let i = 0; i < poolList.length; i++) {
                 // console.log("ETH In: " + toke1 + "  USDC Out: " + toke0Out);
             }
             
-            // LPValueCheck();
+            LPValueCheck(liqPool, 0, decimal0, priceOfOneLP);
             
         })
     } else if (category === "ETH-Stablecoin") {
         const baseAssetPos = poolList[i].baseAsset;
+        let priceOfOneLP;
+        if (baseAssetPos === 0) {
+            priceOfOneLP = LPValueStartup(liqPool, baseAssetPos, decimal0);
+        } else if (baseAssetPos === 1) {
+            priceOfOneLP = LPValueStartup(liqPool, baseAssetPos, decimal1);
+        }
+        
         liqPool.on("Swap", async (sender, amount0In, amount1In, amount0Out, amount1Out, to) => {
             let toke0 = parseFloat(amount0In.toString());
             toke0 = toke0 / (10 ** decimal0);
@@ -118,14 +137,14 @@ for (let i = 0; i < poolList.length; i++) {
             let otherOut;
             let comparativeName;
             let otherName;
-            if (baseAsset === 0) {
+            if (baseAssetPos === 0) {
                 comparativeIn = toke0;
                 comparativeOut = toke0Out;
                 otherIn = toke1;
                 otherOut = toke1Out;
                 comparativeName = name0;
                 otherName = name1;
-            } else if (baseAsset === 1) {
+            } else if (baseAssetPos === 1) {
                 comparativeIn = toke1;
                 comparativeOut = toke1Out;
                 otherIn = toke0;
@@ -141,8 +160,12 @@ for (let i = 0; i < poolList.length; i++) {
                 console.log(`${otherName} In:  ${otherIn}   ${comparativeName} Out:  ${comprativeOut}`);
                 // console.log("ETH In: " + toke1 + "  USDC Out: " + toke0Out);
             }
+            if (baseAssetPos === 0) {
+                LPValueCheck(liqPool, baseAssetPos, decimal0, priceOfOneLP);
+            } else if (baseAssetPos === 1) {
+                LPValueCheck(liqPool, baseAssetPos, decimal1, priceOfOneLP);
+            }
             
-            // LPValueCheck();
             
         })
     }
